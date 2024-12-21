04ae486571a84f2c6ae86566be337eba1b6cfeef
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-12-21 15:31:22 -0300

    test: Define tests based on step 1 requirements

---
+++ tests/test_step_1.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestStep1(unittest.TestCase):
+    def test_step_1__has_necessary_fields(self):
+        pass
+
+    def test_step_1__types_are_correct_and_values_within_range(self):
+        pass
+
+    def test_step_1__duty_ids_are_unique(self):
+        pass
+
+    def test_step_1__all_duty_ids_included(self):
+        pass
+
+    def test_step_1_only_valid_duty_ids_included(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
