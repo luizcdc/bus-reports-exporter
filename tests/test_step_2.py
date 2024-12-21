403cf5765bdb0deedaa2be830c544fd0c9e82851
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-12-21 15:32:56 -0300

    test: Define tests based on step 2 requirements

---
+++ tests/test_step_2.py
@@ -0,0 +1,28 @@
+import unittest
+
+class TestStep2(unittest.TestCase):
+    def test_step_2__has_necessary_fields(self):
+        pass
+
+    def test_step_2__types_are_correct_and_values_within_range(self):
+        pass
+
+    def test_step_2__duty_ids_are_unique(self):
+        pass
+
+    def test_step_2__all_duty_ids_included(self):
+        pass
+
+    def test_step_2__only_valid_duty_ids_included(self):
+        pass
+
+    def test_step_2__no_depot_as_start(self):
+        pass
+
+    def test_step_2__no_depot_as_end(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
