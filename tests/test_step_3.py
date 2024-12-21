9cdbbfd6b4e8da7973203c7a694d110facc6536c
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-12-21 15:33:20 -0300

    test: Define tests based on step 3 requirements

---
+++ tests/test_step_3.py
@@ -0,0 +1,35 @@
+import unittest
+
+class TestStep3(unittest.TestCase):
+    def test_step_3__has_necessary_fields(self):
+        pass
+
+    def test_step_3__types_are_correct_and_values_within_range(self):
+        # Note: The requirements enforce that listed breaks can't last less than 15 minutes
+        pass
+
+    def test_step_3__duty_ids_are_unique(self):
+        pass
+
+    def test_step_3__all_duty_ids_included(self):
+        pass
+
+    def test_step_3__only_valid_duty_ids_included(self):
+        pass
+
+    def test_step_3__no_depot_as_start(self):
+        pass
+
+    def test_step_3__no_depot_as_end(self):
+        pass
+
+    def test_step_3__no_break_before_start_time(self):
+        pass
+
+    def test_step_3__no_break_after_end_time(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
