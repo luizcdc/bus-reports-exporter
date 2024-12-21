910a094ae627cae82d6f2479f18c5d149811fe4b
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-12-21 15:51:30 -0300

    feat: Write method declarations for step 1, 2, and 3

---
+++ reports_exporter.py
@@ -0,0 +1,23 @@
+from pandas import DataFrame
+
+class ReportsExplorer:
+    def __init__(self):
+        pass
+
+    # Step 1
+    def generate_duty_start_end_times_report(self, raw_data: dict) -> DataFrame:
+        pass
+
+    # Step 2
+    def generate_duty_start_end_times_and_stops_report(self, raw_data: dict) -> DataFrame:
+        pass
+
+    # Step 3
+    def generate_duty_breaks_report(self, raw_data: dict) -> DataFrame:
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
